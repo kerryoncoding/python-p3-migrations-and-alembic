52a927afdc0a6aff49aedab2032c45ae4841acb9
Kerry Guarino <kerry.guarino@gmail.com>
2023-10-01 07:47:00 -0400

    walked through alembic migration

---
+++ lib/migrations/versions/ce61a4670d59_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: ce61a4670d59
+Revises: 
+Create Date: 2023-10-01 07:32:48.963139
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ce61a4670d59'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
